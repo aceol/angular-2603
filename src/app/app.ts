0c685c6efb754fe9ddd6f9eb7e87bf6dceca9391
Antoine Ceol <antoine.ceol@gmail.com>
2026-03-11 23:17:09 +0100

    Lab 4 - Components 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,61 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductCard } from './product-card/product-card';
+import { Product } from './product-card/product';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   isHovered = false;
   protected readonly title = 'Zenika Shop'; //signal('zenika-ng-website');
-
+  total = 0;
+  cartItems = 0;
   toggleIsHovered($event: any): void {
     console.log($event);
     this.isHovered = !this.isHovered;
   }
+
+  updateTotal(product: Product) {
+    this.total += product.price;
+    this.cartItems++;
+  }
+
+  products = [
+    {
+      id: 'welsch',
+      title: 'Coding the welsch',
+      description: 'Tee-shirt col rond - Homme',
+      photo: '/assets/coding-the-welsch.jpg',
+      price: 20,
+      stock: 2,
+    },
+    {
+      id: 'world',
+      title: 'Coding the world',
+      description: 'Tee-shirt col rond - Homme',
+      photo: '/assets/coding-the-world.jpg',
+      price: 18,
+      stock: 1,
+    },
+    {
+      id: 'vador',
+      title: 'Duck Vador',
+      description: 'Tee-shirt col rond - Femme',
+      photo: '/assets/coding-the-stars.jpg',
+      price: 21,
+      stock: 2,
+    },
+    {
+      id: 'snow',
+      title: 'Coding the snow',
+      description: 'Tee-shirt col rond - Femme',
+      photo: '/assets/coding-the-snow.jpg',
+      price: 19,
+      stock: 2,
+    },
+  ];
 }
---
+++ src/app/product-card/product-card.ts
@@ -0,0 +1,13 @@
+import { Component, input, output } from '@angular/core';
+import { Product } from './product';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [],
+  templateUrl: './product-card.html',
+  styleUrl: './product-card.css',
+})
+export class ProductCard {
+  product = input.required<Product>();
+  addToBasket = output<Product>();
+}
